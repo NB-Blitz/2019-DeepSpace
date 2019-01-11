c0a0555206e3caf403cdca42163b672ccc86b401
SamzFerg <SamzFerg@gmail.com>
2019-01-10 19:01:48 -0600

    Added Vendor dependencies and added BlitzLib Include Statement

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -7,24 +7,17 @@
 
 #pragma once
 
+//C++ Library Include
 #include <string>
 
-#include <frc/Joystick.h>
-#include <frc/PWMVictorSPX.h>
-#include <frc/SampleRobot.h>
-#include <frc/drive/DifferentialDrive.h>
-#include <frc/smartdashboard/SendableChooser.h>
+//WPILIB Include
+#include <WPILib.h>
+
+//Blitz Lib Include
+#include "blitzlib/BlitzLib.hpp"
 
 /**
- * This is a demo program showing the use of the DifferentialDrive class. The
- * SampleRobot class is the base of a robot application that will automatically
- * call your Autonomous and OperatorControl methods at the right time as
- * controlled by the switches on the driver station or the field controls.
- *
- * WARNING: While it may look like a good choice to use for your code if you're
- * inexperienced, don't. Unless you know what you are doing, complex code will
- * be much more difficult under this system. Use TimedRobot or Command-Based
- * instead if you're new.
+
  */
 class Robot : public frc::SampleRobot 
 {
